34145c53000fe282588c09e637568ed7c3897b3e
Chris <ChrisRodea@gmail.com>
2022-07-07 21:43:34 -0700

    Update CDI_inventory_reader.py
    created a function to get the quantity requested from the CDI order pdf. Also included the license number on the inventory sheet that is created

--- CDI_inventory_reader.py
+++ CDI_inventory_reader.py
@@ -5,8 +5,14 @@ from csv import DictWriter
 import csv
 import os
 
+
+##! NEED TO PULL LICENSE AS WELL 
+""" Need to know if it is the whole lot or not .If it is not the whole lot, then they (juan) needs to go into the lot and get the individual licenses and put the pallets not on the order on hold, so they dont get pulled. If it is the whole """
+
 #! This is the directory where the inventory file is stored 
-inventory_directory = "C:\\Users\\Chris R\\Desktop\\Python Projects\\Mini_Projects\\cdi_reader"
+inventory_directory = "C:\\Users\\Chris R\\Desktop\\Python Projects\\Mini_Projects\\cdi_reader"  #! Use this at home
+##inventory_directory = "C:\\Users\\crodea\\Desktop\\\Mini_Projects\\cdi_reader"  #! Use this at work 
+#inventory_directory = "C:\\Users\\crodea\\Desktop\\OneDrive\\OneDrive - US Cold Storage\\Python\\CDI_allocations_project" #! One drive folder
 #print(file_directory)
 
 #!This is the directory that holds the allocation PDF's 
@@ -15,12 +21,12 @@ path = "C:\\Users\\Chris R\\Desktop\\Python Projects\\Mini_Projects\\cdi_reader\
 
 
 #! Open Up file and get the contents of the first page 
-def get_text(cdi_pdf):
-    pdfFileObj = open('CDI1.pdf', 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    pageObj = pdfReader.getPage(0)
-    text = pageObj.extractText()
-    return text
+# def get_text(cdi_pdf):
+#     pdfFileObj = open('CDI1.pdf', 'rb')
+#     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+#     pageObj = pdfReader.getPage(0)
+#     text = pageObj.extractText()
+#     return text
 
 #! This will get the total number of pages for each pdf. 
 def get_page_cnt(pdf): #! example of possible input ==> ['CDI0.pdf', 'CDI1.pdf', 'CDI2.pdf']
@@ -32,14 +38,30 @@ def get_page_cnt(pdf): #! example of possible input ==> ['CDI0.pdf', 'CDI1.pdf',
 
 
 #! This function will find the order Number located in the PDF and store it in a variable to be used later as the output csv file name
-def getOrderNumber():
-    pass
+def getOrderNumber(pdf):
+    pattern = re.compile("SO000\\d\\d\\d\\d\\d\\d", re.IGNORECASE)
+    match = pattern.findall(pdf)
+    return match[0] 
 
 #! Gets the pallet ID from the CDI allocation PDF file that is opened 
 def getPalletId(cdi_pallets):
     pattern = re.compile("\\d\\d\\d\\d-\\d\\d\\d-\\d\\d-[a-zA-Z]\\d\\d-[a-zA-Z]", re.IGNORECASE)
     match = pattern.findall(cdi_pallets)
     return match #! Returns a list of pallet ID's ==> ['3822-153-08-S04-B', '6622-153-13-S02-F', '6622-153-13-S03-C', '6622-153-13-S04-B']
+
+#! Gets the quantity that is requested on the CDI order manifest. *NOT* the quantity in inventory
+def getQTY(input):
+    pattern = re.compile("(\d+\.0000\s+CS)", re.MULTILINE)
+    match = pattern.findall(input) #! Returns ['84.0000\n\n\nCS', '84.0000\n\n\nCS', '84.0000\nCS']
+    
+    qty = []
+    for items in match:
+        strip_new_line = items.split("\n") #! Returns ['84.0000', '', '', 'CS']
+        add_CS = strip_new_line[0] + " CS" #! Returns 84.0000 CS
+        
+        qty.append(add_CS)
+    
+    return qty
 
 #! Renames all the cdi pdfs in the folder 'test_allocations' so that we can loop through them 
 def rename_files():
@@ -69,11 +91,9 @@ def rename_files():
 
     return file_list
 
-
-#PID = getPalletId(text)
 #! Opens the inventory excel sheet (8-3-1 report in WMS). looks up each pallet ID that is in the PID list
 #! found in the getPalletId() function
-def read_from_inventory_csv(number):
+def read_from_inventory_csv(number, qty):
     #csv_file = csv.reader(open("inventory.csv", "r"), delimiter=",") #! need to open the file within the for loop, or else it will not restart the search from top of file
     row_info = []
     os.chdir(inventory_directory) #! Change back from the pdf folder.
@@ -81,6 +101,8 @@ def read_from_inventory_csv(number):
     for items in range(len(number)):
         csv_file = csv.reader(open("inventory.csv", "r"), delimiter=",")
         palletID = number[items]
+        qty_requested = qty[items]
+        #print(number)
         add_comma = "'" + str(palletID) #! Turns 3822-150-05-S01-D into '3822-150-05-S01-D . Because the inventory csv adds a comma in front of PID
         #print(add_comma)
         for row in csv_file:
@@ -90,21 +112,21 @@ def read_from_inventory_csv(number):
                 info = {
                     "Product Code": " ".join(row[2].split()),
                     "Lot Number": " ".join(row[1].split()), #! Strips the extra spaces 
-                    "Quantity": " ".join(row[8].split()),
+                    "Quantity Requested From CDI": qty_requested, 
+                    "Quantity in INV": " ".join(row[8].split()),
                     "Batch ID": palletID,
                     "Pallet ID Matched": " ".join(row[13].split()),
+                    "License Number": " ".join(row[12].split())
                 }
 
                 row_info.append(info) #! Adds to the row_info list, which the function will return later. 
                 break
             else:
+                """Need to create a template for product that doesn't have a match """
                 #print(str(palletID) + " No match " + str(row[13]))
                 continue
         continue
 
-    #rm_extra_spaces = " ".join(palletInfo.split())
-    #convert_to_list = rm_extra_spaces.split(" ")
-    #print(row_info)
     return row_info
 
 
@@ -121,13 +143,19 @@ def main():
         pdfFileObj = open(f'{invoice}', 'rb')
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
         pages = pdfReader.getNumPages()
+        pageObj = pdfReader.getPage(0)
+        text = pageObj.extractText()    
+
+        orderNumber = getOrderNumber(text)
+
         #print(pages)
         myPalletList = [] #! Stores all the pallet ID's from each page of CDI pdf. 
 
         for page in range(pages):
             pageObj = pdfReader.getPage(page)
             text = pageObj.extractText()
-            PID = getPalletId(text)
+            PID = getPalletId(text) #! Gets all the Pallet ID on Manifest
+            QTY_REQUESTED = getQTY(text) #! Gets all the qty requested on Manifest
             if len(PID) == 0: #! If any page is empty or has no pallet ID, program will move on to next page
                 continue
             else:
@@ -135,35 +163,13 @@ def main():
                     myPalletList.append(palletID)
                 #print(myPalletList)
         
-        matched_pallets = read_from_inventory_csv(myPalletList)
+        matched_pallets = read_from_inventory_csv(myPalletList, QTY_REQUESTED)
         
-        with open(f'{pdf}.csv','w', newline='') as outfile:
-                writer = DictWriter(outfile, ('Product Code','Lot Number','Quantity', 'Batch ID', 'Pallet ID Matched'))
+        with open(f'{orderNumber}.csv','w', newline='') as outfile:
+                writer = DictWriter(outfile, ('Product Code','Lot Number','Quantity Requested From CDI','Quantity in INV', 'Batch ID', 'Pallet ID Matched', 'License Number'))
                 writer.writeheader()
                 writer.writerows(matched_pallets)
             
-        #print(myPalletList)
-        #print(pdf)
-                
-        #!!!!! THIS IS WHERE YOU LEFT OFF 
-        #read_from_inventory_csv(myPalletList)    
-            
-            # print(pdf)
-            # print("Page: " + str(page))
-            # print(PID)
-
-            
-    # myPalletList = []
-    # #print(PID)
-    # #print(inventory)
-    # print(pdfs[1])
-    # #print(os.getcwd()){pdfs[1]}
-
-    # with open(f'{pdfs[1]}.csv','w') as outfile:
-    #     writer = DictWriter(outfile, ('Product Code','Lot Number','Quantity', 'Batch ID', 'Pallet ID Matched'))
-    #     writer.writeheader()
-    #     writer.writerows(inventory)
-        
     
 
 main()
