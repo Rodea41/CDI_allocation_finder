8e06dcce1f9caf91e330f6ed267f6bcf2a01fae1
Chris <ChrisRodea@gmail.com>
2022-07-10 16:47:37 -0700

    Update CDI_inventory_reader.py
    Program now takes the outputed csv sheet and converts it to xlsx format. Currently working on formatting the xlsx sheet with color

--- CDI_inventory_reader.py
+++ CDI_inventory_reader.py
@@ -1,9 +1,9 @@
 
 import PyPDF2
-import re
+import re, csv, os
 from csv import DictWriter
-import csv
-import os
+import pandas as pd
+import openpyxl
 
 
 ##! NEED TO PULL LICENSE AS WELL 
@@ -14,6 +14,10 @@ inventory_directory = "C:\\Users\\Chris R\\Desktop\\Python Projects\\Mini_Projec
 ##inventory_directory = "C:\\Users\\crodea\\Desktop\\\Mini_Projects\\cdi_reader"  #! Use this at work 
 #inventory_directory = "C:\\Users\\crodea\\Desktop\\OneDrive\\OneDrive - US Cold Storage\\Python\\CDI_allocations_project" #! One drive folder
 #print(file_directory)
+
+#! This is the inventory file
+inventory_file = "inventory.csv"
+
 
 #!This is the directory that holds the allocation PDF's 
 #path = "C:\\Users\\crodea\\Desktop\\test_allocations"  #! This is the path for work desktop
@@ -97,9 +101,9 @@ def read_from_inventory_csv(number, qty):
     #csv_file = csv.reader(open("inventory.csv", "r"), delimiter=",") #! need to open the file within the for loop, or else it will not restart the search from top of file
     row_info = []
     os.chdir(inventory_directory) #! Change back from the pdf folder.
-    
+
     for items in range(len(number)):
-        csv_file = csv.reader(open("inventory.csv", "r"), delimiter=",")
+        csv_file = csv.reader(open(f"{inventory_file}", "r"), delimiter=",")
         palletID = number[items]
         qty_requested = qty[items]
         #print(number)
@@ -125,10 +129,76 @@ def read_from_inventory_csv(number, qty):
                 """Need to create a template for product that doesn't have a match """
                 #print(str(palletID) + " No match " + str(row[13]))
                 continue
+
         continue
 
     return row_info
 
+
+#! This func is used to get all the lots that are being requested. We will use this list that is returned to check against what is being requested
+def get_entire_lot(list_of_pallets):
+    all_of_lot = []     #* Returns ['90786', '90786', '34924', '34924', '33964', '33964', '33964', '97884', '5630', '92', '92', '92', '1127', '1127', '1127', '1127', '1127', '1127', '1127'] . Shows duplicates
+    for items in list_of_pallets:
+        #print(items.get('Lot Number'))
+        lot_num = items.get('Lot Number')
+        all_of_lot.append(lot_num)
+    #print(all_of_lot)
+
+    rm_dups = set(all_of_lot) #* Returns {'91619', '91445', '97562', '91446'} . Removes all duplicates in list 'all_of_lot'
+    #print(rm_dups)
+    bk_to_list = list(rm_dups)
+
+    return bk_to_list
+
+
+
+#! This function strips all the spaces in the inventory excel sheet
+def trim_all_columns(df):
+    """
+    Trim whitespace from ends of each value across all series in dataframe
+    """
+    trim_strings = lambda x: x.strip() if isinstance(x, str) else x
+    return df.applymap(trim_strings)
+
+
+#! Takes the list returned from func 'get_entire_lot' and looks get everything in that lot in the inventory csv
+def read_lots_from_csv(lots):
+    os.chdir(inventory_directory) #! Change back from the pdf folder.
+    
+    lot_info =[]
+    
+    #for items in range(len(lots)):
+    for items in lots:
+        #print(items)
+        df = pd.read_csv(f"{inventory_file}")
+        df = trim_all_columns(df)
+        gk = df.groupby(['Lot','Pallet Id'])
+        gk = df.loc[df["Lot"] == f"{items}"]
+        to_dictionary = gk.to_dict('records')
+    #gk = df.groupby(['Pallet Id', 'Lot'])
+    #print(gk.get_group('91619'))
+        filter_dictionary = []
+        for row in to_dictionary:
+            #print(row)
+            relevant_info = {
+                        "Lot": row.get('Lot'),  
+                        "Pallet ID Matched": row.get('Pallet Id'),
+                        "License Number": row.get('License  ')
+                        }
+            filter_dictionary.append(relevant_info)
+        
+        lot_info.append(filter_dictionary)
+        #lot_info.append(to_dictionary)
+    #print(lot_info)
+    return lot_info
+
+    #print(lots[0])
+    # match = df.set_index(['Lot'])
+    #match1 = match[match['Lot'] == '91445']
+    #match = df.loc[df['Lot'] == '3592']
+    #print(match)
+    
+    #by_lot = df.groupby(["Lot"]())
 
 
 
@@ -164,12 +234,37 @@ def main():
                 #print(myPalletList)
         
         matched_pallets = read_from_inventory_csv(myPalletList, QTY_REQUESTED)
-        
+        lots = get_entire_lot(matched_pallets)
+        #print(lots)
+        #!!! CURRENTLY WORKING ON THIS PART . LOOKING HOW TO GROUP LOOKUP ALL LOTS AVAILABLE.
+        all_pallets_in_lots = read_lots_from_csv(lots)
+
         with open(f'{orderNumber}.csv','w', newline='') as outfile:
                 writer = DictWriter(outfile, ('Product Code','Lot Number','Quantity Requested From CDI','Quantity in INV', 'Batch ID', 'Pallet ID Matched', 'License Number'))
                 writer.writeheader()
                 writer.writerows(matched_pallets)
-            
+                outfile.write("\n")
+        
+        #! This writes all the palletID's that are in the lot, this will help show if the order is using the whole lot or if product is left over. 
+        with open(f'{orderNumber}.csv','a', newline='') as outfile:
+            for items in range(len(all_pallets_in_lots)):
+                    writer = DictWriter(outfile, ('Lot','Pallet ID Matched','License Number'))
+                    writer.writeheader()
+                    writer.writerows(all_pallets_in_lots[items])
+                    outfile.write("\n")
     
+        
+        #! This converts the csv file to an xlsx file that can be formatted with color. 
+        wb = openpyxl.Workbook()
+        ws = wb.active
+
+        with open(f'{orderNumber}.csv') as f:
+            reader = csv.reader(f, delimiter=',')
+            for row in reader:
+                ws.append(row)
+
+        wb.save(f'{orderNumber}.xlsx')
+
 
 main()
+
